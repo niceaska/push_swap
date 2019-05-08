5948d94e78e8fb62a2a28c14220cc4e817eabbdb
niceaska <niceaska@gmail.com>
2019-05-08 19:33:58 +0300

    add ps main

--- push_swap.c
+++ push_swap.c
@@ -1,17 +1,39 @@
 #include "push-swap.h"
 
-int	main(int ac, char **ag)
+static t_env *init_env(void)
 {
-	int *arr;
-	t_stack *sa = init_stack();
-	t_stack *sb = init_stack();
-	if (!(arr = check_args(ag, ac, &sa, 0)))
-		print_error(sa, sb);
-	
+	t_env *e;
 
+	if (!(e = (t_env *)malloc(sizeof(t_env))))
+		return (NULL);
+	e->s_a = init_stack();
+	e->s_b = init_stack();
+	return (e);
+}
 
-	mainsort(sa, sb, arr, ac -1);
-	free_stack(sa);
-	free_stack(sb);
+int		main(int ac, char **ag)
+{
+	t_env			*e;
+	unsigned int	size;
+	int				*arr;
+
+	if (ac == 1)
+		return (0);
+	if (!(e = init_env()))
+		print_error(NULL, NULL, NULL);
+	if (!(arr = check_args(ag, ac, &(e->s_a), 0)))
+		print_error(e->s_a, e->s_b, e);
+	size = stack_size(e->s_a);
+	if (is_sorted(e->s_a, 0, size))
+		return (0);
+	if (size <= 3)
+		sort_tree(e->s_a, e->s_b);
+	else if (size <= 10)
+		sel_sort(e->s_a, e->s_b);
+	else
+		mainsort(e->s_a, e->s_b, arr, ac -1);
+	free_stack(e->s_a);
+	free_stack(e->s_b);
+	free(e);
 	free(arr);
 }
